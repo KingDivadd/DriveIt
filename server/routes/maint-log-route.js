877c65db45fdab14e2ce40ccdf3038884dcc68ae
Iroegbu David <iroegbu.dgit@gmail.com>
2024-02-11 21:28:40 +0100

    updated maint log

--- server/routes/maint-log-route.js
+++ server/routes/maint-log-route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { allVehicleMaintLog, createVehicleMaintLog, editVehicleMaintLog, planMaint, editPlannedMaint, allPlannedMaint, onePlannedMaint, addMaintPersonnelFeedback, editMaintPersonnelFeedback, addVehicleOwnersFeedback, updatePlannedMaintStatus, allMaintLog } = require('../controllers/maint-log-controller')
+const { allVehicleMaintLog, createVehicleMaintLog, editVehicleMaintLog, planMaint, editPlannedMaint, allPlannedMaint, onePlannedMaint, addMaintPersonnelFeedback, editMaintPersonnelFeedback, addVehicleOwnersFeedback, updatePlannedMaintStatus, allMaintLog, allVehiclesPlannedMaint } = require('../controllers/maint-log-controller')
 const tokenDecoder = require("../middleware/auth-middleware")
 
 router.route("/all-maint-log").post(tokenDecoder, allVehicleMaintLog) // for registered user
@@ -12,6 +12,7 @@ router.route('/plan-maint').post(tokenDecoder, planMaint)
 router.route('/edit-planned-maint').patch(tokenDecoder, editPlannedMaint)
 router.route('/update-maint-status').patch(tokenDecoder, updatePlannedMaintStatus)
 router.route('/all-planned-maint').post(tokenDecoder, allPlannedMaint)
+router.route('/all-vehicles-planned-maint').post(tokenDecoder, allVehiclesPlannedMaint)
 router.route('/all-planned-maint/:id').post(tokenDecoder, onePlannedMaint)
 router.route('/maintenance-feedback').patch(tokenDecoder, addMaintPersonnelFeedback)
 router.route('/edit-maintenance-feedback').patch(tokenDecoder, editMaintPersonnelFeedback)
